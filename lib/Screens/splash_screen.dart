9a47ef43cae6744377b9af9e6ae8bfd64c02f69d
Mikko Jed Tan <89184826+mjt1400@users.noreply.github.com>
2021-10-21 15:51:21 +0800

    splash screen

---
+++ lib/Screens/splash_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:twitter/Screens/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    var d = const Duration(seconds: 5);
+
+    Future.delayed(d, () {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) {
+          return const LoginScreen();
+        }),
+        (route) => false,
+      );
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF006064),
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                Image.asset(
+                  'assets/logo.png',
+                  height: 300,
+                  width: 300,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
